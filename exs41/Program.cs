3635977f1b405e3cf7e5ceed08315da7470deff3
m.gorshkov <m.gorshkov@skbkontur.ru>
2022-08-24 20:29:57 +0800

    add ex41

--- exs41/Program.cs
+++ exs41/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+using System;
 
 namespace exs41
 {
@@ -6,7 +7,33 @@ namespace exs41
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите количество чисел: ");
+            int amount=Convert.ToInt32(Console.ReadLine());
+           int[] arr=new int[amount];
+           int posNum=0;
+            for (int i=0; i<amount; i++)
+            {
+                 Console.WriteLine("Введите число: ");
+                 arr[i] = Convert.ToInt32(Console.ReadLine());
+                 if (arr[i]> 0)
+                 {
+                    posNum=posNum+1;
+                 }
+                 
+            }
+            Console.WriteLine("Введенный массив: ");
+            for (int i=0; i<arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} " );
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Количество чисел больше нуля= {posNum} ");
+
+
+
+        
+             
+            
         }
     }
 }
