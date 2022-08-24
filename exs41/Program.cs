7f9120b206cccd54c23b4ff2b60e61828532578e
m.gorshkov <m.gorshkov@skbkontur.ru>
2022-08-24 19:16:41 +0800

    add ignore

---
+++ exs41/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exs41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
